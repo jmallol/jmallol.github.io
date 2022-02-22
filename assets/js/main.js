21739461ff82929f0d9b7f0c1c22466016db1b57
Jesus <73107294+jmallol@users.noreply.github.com>
2022-02-22 21:31:25 +0100

    Add files via upload
    commit 22-2-2022

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,6 +31,22 @@
 			}, 100);
 		});
 
+	// Collapsible Content.
+		var coll = document.getElementsByClassName("collapsible");
+		var i;
+		
+		for (i = 0; i < coll.length; i++) {
+		  coll[i].addEventListener("click", function() {
+			this.classList.toggle("active");
+			var content = this.nextElementSibling;
+			if (content.style.display === "block") {
+			  content.style.display = "none";
+			} else {
+			  content.style.display = "block";
+			}
+		  });
+		}
+
 	// Fix: Flexbox min-height bug on IE.
 		if (browser.name == 'ie') {
 
